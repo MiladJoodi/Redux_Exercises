df2363351ab4f1a9d322020c5492b2d7f849d9ab
Milad Joodi <winbeta.com@gmail.com>
2024-01-08 18:00:25 +0330

    Update app.js

--- 00/js/app.js
+++ 00/js/app.js
@@ -1,5 +1,19 @@
-// console.log(Redux)
 const decBtn = document.getElementById('.dec')
 const resetBtn = document.getElementById('.reset')
 const incBtn = document.getElementById('.inc')
-const number = document.getElementById('#num')
+const numberElem = document.getElementById('#num')
+
+//Declare Counter Reducer
+const counterReducer = (action)=>{
+    switch(action.type){
+        case 'INCREMENT':{
+
+        }
+        case 'DECREMENT':{
+            
+        }
+        case 'SET_ZERO':{
+            
+        }
+    }
+}
