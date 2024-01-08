f2b5e30e0e87180377bcc73bd7a1d4f1912c4c33
Milad Joodi <winbeta.com@gmail.com>
2024-01-08 18:31:03 +0330

    update

--- 00/js/app.js
+++ 00/js/app.js
@@ -4,19 +4,33 @@ const incBtn = document.getElementById('.inc')
 const numberElem = document.getElementById('#num')
 
 //Declare Counter Reducer
-const counterReducer = (action)=>{
-    switch(action.type){
-        case 'INCREMENT':{
-
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'INCREMENT': {
+            return state + 1
         }
-        case 'DECREMENT':{
-            
+        case 'DECREMENT': {
+            return state - 1
         }
-        case 'SET_ZERO':{
-            
+        case 'SET_ZERO': {
+            // return 0
+            return store - store
         }
-        default:{
-            
+        default: {
+            return state
         }
     }
 }
+
+const store = Redux.createStore(counterReducer)
+
+//Handle Events
+decBtn.addEventListener('click', () => {
+
+})
+resetBtn.addEventListener('click', () => {
+
+})
+incBtn.addEventListener('click', () => {
+
+})
