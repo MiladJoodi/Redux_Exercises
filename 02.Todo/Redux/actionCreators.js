b085b51713544bf4a9b55f65b4b0240c5b3cf98e
Milad Joodi <winbeta.com@gmail.com>
2024-01-09 21:40:13 +0330

    Update actionCreators.js

--- 02.Todo/Redux/actionCreators.js
+++ 02.Todo/Redux/actionCreators.js
@@ -1,28 +1,41 @@
-import {addTodo,removeTodo,doTodo} from './actions.js'
+import { addTodo, removeTodo, doTodo, filterAllTodos, filterCompletedTodos, filterIncompletedTodos } from './actions.js'
 
-const addTodoAction = (value)=>{
-    return{
+const addTodoAction = (value) => {
+    return {
         type: addTodo,
         title: value
     }
 }
-const removeTodoAction = (id)=>{
-    return{
+const removeTodoAction = (id) => {
+    return {
         type: removeTodo,
         id
     }
 }
 
-const doTodoAction = (id)=>{
-    return{
+const doTodoAction = (id) => {
+    return {
         type: doTodo,
         id
     }
 }
 
-export{
+const filterAllTodos = () => {
+    return {
+        type: filterAllTodos,
+    }
+}
+const filterCompletedTodosAction = () => {
+    return {
+        type: filterCompletedTodos,
+    }
+}
+
+
+
+export {
     addTodoAction,
     removeTodoAction,
     doTodoAction,
-    
+
 } 
