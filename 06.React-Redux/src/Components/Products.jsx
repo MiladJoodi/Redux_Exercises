3aa070716908f1b81784d7143bc0a37c998e47d3
Milad Joodi <winbeta.com@gmail.com>
2024-01-13 00:17:42 +0330

    new day

--- 06.React-Redux/src/Components/Products.jsx
+++ 06.React-Redux/src/Components/Products.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 function Products(props) {
+  console.log("props: " , props)
   return (
     <ul>
         Product List:
-        <li>Product 1</li>
-        <li>Product 2</li>
+        {
+          props.products.map((product=> (
+            <li>{product.title}</li>
+          )))
+        }
     </ul>
   )
 }
 
 function mapStateToProps(state){
     console.log('store', state);
-    return {}
+    return {
+      products: state.products
+    }
 }
 
 export default connect(mapStateToProps)(Products);
