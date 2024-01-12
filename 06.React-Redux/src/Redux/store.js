14f74d87514cc5e588a685d57189625201ba73fd
Milad Joodi <winbeta.com@gmail.com>
2024-01-12 23:37:06 +0330

    daryei

---
+++ 06.React-Redux/src/Redux/store.js
@@ -0,0 +1,14 @@
+import { legacy_createStore, combineReducers } from 'redux';
+import reducers from "./reducer"
+
+import productReducer from './store/Products'
+import articlesReducer from './store/Products'
+import commentReducer from './store/Products'
+
+const store = legacy_createStore(combineReducers({
+    productReducer,
+    articlesReducer,
+    commentReducer,
+}))
+
+export default store;
--- 06.React-Redux/src/main.jsx
+++ 06.React-Redux/src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
 
+import store from './Redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-const render = ()=> root.render(
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
-render()
-store.subscribe(render)
