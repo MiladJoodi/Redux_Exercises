d113d0f11fcafb3e5943b9d413f9d9a92c30a5f0
Milad Joodi <winbeta.com@gmail.com>
2024-01-12 17:22:30 +0330

    12

--- 04/src/js/app.js
+++ 04/src/js/app.js
@@ -3,17 +3,17 @@ import articlesReducer from "./../Redux/Articles/reducer.js"
 import commentsReducer from "./../Redux/Comments/reducer.js"
 
 import {addProductAction} from "./../Redux/Products/actionCreators.js"
+import {addArticlesAction} from "./../Redux/Articles/actionCreators.js"
 
 const store = Redux.createStore(
-  Redux.commentsReducer({
+  Redux.commentsReducers({
     products: productsReducer,
     articles: articlesReducer,
     comments: commentsReducer,
   })
 )
 
-console.log(store.getState())
+store.subscribe(()=> console.log(`Store updated: ${store.getState()}`))
 
 store.dispatch(addProductAction( {id: 1, title: "Laptop", price: "90_000_000"}));
 
-console.log(store.getState())
