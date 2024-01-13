31f6ab7a7ec797625d5523f1dcdede332665ec9e
Milad Joodi <winbeta.com@gmail.com>
2024-01-13 15:14:17 +0330

    ♥

--- 07.Redux-Toolkit/src/Redux/store.js
+++ 07.Redux-Toolkit/src/Redux/store.js
@@ -1,7 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit';
 
-// import { composeWithDevTools } from '@reduxjs/toolkit/dist/devtoolsExtension';
-
 import todosReducer from "./store/Todos";
 
 const store = configureStore({
