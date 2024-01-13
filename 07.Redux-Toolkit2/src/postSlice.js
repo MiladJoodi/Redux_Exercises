36c1cab757816fa18fdb5da5af1da18718b6165f
Milad Joodi <winbeta.com@gmail.com>
2024-01-13 18:40:13 +0330

    hard hartd

--- 07.Redux-Toolkit2/src/postSlice.js
+++ 07.Redux-Toolkit2/src/postSlice.js
@@ -1,10 +1,10 @@
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 
-const fetchPosts = createAsyncThunk('posts/fetch', 
+export const fetchPosts = createAsyncThunk('posts/fetch', 
 async ()=>{
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await response.json();
-    
+
     return data;
 })
 
@@ -17,4 +17,19 @@ initialStati = {
 const postSlice = createSlice({
     name: 'posts',
     initialState,
+    extraReducers:{
+        [fetchPosts.fulfilled]: (state, action)=> {
+            state.postList = action.payload
+            state.fetchingPost = false
+        },
+        [fetchPosts.pending]: (state)=> {
+            state.fetchingPost = true
+        },
+        [fetchPosts.rejected]: (state)=>{
+            state.fetchingPost = false,
+            state.errorMessage = "ُsomething went wrong"
+        }
+    }
 })
+
+export default postSlice.reducer;
